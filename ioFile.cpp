cedd768cf24b545430722490e89bf12e28b8419a
Ghatan55 <ghatanhardanni@gmail.com>
2025-06-09 14:37:13 +0700

    membuat objek output file stream dan membuka file untuk ditulisi

--- ioFile.cpp
+++ ioFile.cpp
@@ -3,3 +3,16 @@
 #include <string>
 using namespace std;
 
+int main() {
+    string baris;
+
+    // membuat objek output file stream
+    ofstream outfile;
+    // membuka file untuk ditulisi
+    outfile.open("contohfile.txt");
+
+    cout << " >= Menulis file, \'q\' untuk keluar" << endl;
+
+
+    
+};
