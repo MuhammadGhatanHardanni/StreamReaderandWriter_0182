ee8f04a8b9742f9c1db8e546a5faa8d2f1df2292
Ghatan55 <ghatanhardanni@gmail.com>
2025-06-09 14:57:02 +0700

    Membuat perulangan

--- ioFile.cpp
+++ ioFile.cpp
@@ -33,5 +33,19 @@ int main() {
     infile.open("contohfile.txt");
     
     cout << endl << ">= Membuka dan membaca file " << endl;
-    
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        // Melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesasi
+        infile.close();
+    }
+    // jika tidak menemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 };
