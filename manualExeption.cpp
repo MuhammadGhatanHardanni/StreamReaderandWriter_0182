b8779dcbfcb52fd40acd3c154848d40187b9f56f
Ghatan55 <ghatanhardanni@gmail.com>
2025-06-09 15:19:05 +0700

    Membuat try catch exeption

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -3,5 +3,20 @@ using namespace  std;
 
 int main()
 {
-    
+    try {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        throw 3.5; // melemparkan sebuah integrar maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << a << " : Pengecualian akan dieksekusi" << endl;
+
+    }
+    catch (...) {
+        // jika selain integrar maka block ini akan dieksekusi
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
 }
